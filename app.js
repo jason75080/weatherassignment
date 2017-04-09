5935b35f7c32bd28f431504d2f7e1c979e5dc061
jason75080 <jason75080@gmail.com>
2017-04-09 02:00:17 +0000

    updated app and directive

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-///// WEATHER SERVICE APP //////////////////////////////////////////////////////
-var myModule = angular.module('WeatherServiceApp', ['angular-skycons']);
+///// WEATHER APP //////////////////////////////////////////////////////
+var myModule = angular.module('WeatherApp', ['angular-skycons']);
 
-///// WEATHER SERVICE CONTROLLER ///////////////////////////////////////////////
-myModule.controller('WeatherServiceController', 
+///// WEATHER CONTROLLER ///////////////////////////////////////////////
+myModule.controller('WeatherController', 
                 ['$scope', '$http', 'WUWeatherService', 'DSWeatherService',
                  'GoogleGeolocationService', function($scope, $http, WUWeatherService, 
                     DSWeatherService, GoogleGeolocationService){
     
     //refer to the controller via a variable
-    var wsc = this;
+    var wc = this;
 
     //App name    
-    wsc.app_name = "Weather App";
+    wc.app_name = "Weather App";
 
     //cities to populate the drop-down box
-    wsc.cities = 
+    wc.cities = 
     [
         {
             name: "Amarillo",
@@ -68,96 +68,97 @@ myModule.controller('WeatherServiceController',
         }       
     ];
     
+    
     ///// GET WEATHER UNDERGROUND //////////////////////////////////////////////
-    wsc.getWUCurrentConditions = function(){
-        WUWeatherService.getCurrentConditions(wsc.selected_city)
+    wc.getWUCurrentConditions = function(){
+        WUWeatherService.getCurrentConditions(wc.selected_city)
             .then(function(res){
 
                 //load things from the service
-                wsc.wu_weather = res.data.current_observation;
+                wc.wu_weather = res.data.current_observation;
                 
                 //load variables
-                wsc.wu_temperature_string = wsc.wu_weather.temperature_string;
-                wsc.wu_observation_time = wsc.wu_weather.observation_time;
-                wsc.wu_temperature_string = wsc.wu_weather.temperature_string;
-                wsc.wu_dewpoint_string = wsc.wu_weather.dewpoint_string;
-                wsc.wu_wind_string = wsc.wu_weather.wind_string;
-                wsc.wu_icon_url = wsc.wu_weather.icon_url;
+                wc.wu_temperature_string = wc.wu_weather.temperature_string;
+                wc.wu_observation_time = wc.wu_weather.observation_time;
+                wc.wu_temperature_string = wc.wu_weather.temperature_string;
+                wc.wu_dewpoint_string = wc.wu_weather.dewpoint_string;
+                wc.wu_wind_string = wc.wu_weather.wind_string;
+                wc.wu_icon_url = wc.wu_weather.icon_url;
                 
 
             }).catch(function(error){
               console.log(error);
             });        
-    }
+    };
     
     ///// GET DARK SKY /////////////////////////////////////////////////////////    
-    wsc.getDSCurrentConditions = function(){
-        DSWeatherService.getCurrentConditions(wsc.selected_city)
+    wc.getDSCurrentConditions = function(){
+        DSWeatherService.getCurrentConditions(wc.selected_city)
             .then(function(res){
                 
                 //load things from the service
-                wsc.ds_weather = res.data.currently;
+                wc.ds_weather = res.data.currently;
                 
                 //load variables
-                wsc.ds_temperature      = wsc.ds_weather.temperature;
-                wsc.ds_dewpoint         = wsc.ds_weather.dewPoint;
-                wsc.ds_windBearing      = wsc.ds_weather.windBearing;
-                wsc.ds_windSpeed        = wsc.ds_weather.windSpeed;
-                wsc.ds_observation_time = new Date(wsc.ds_weather.time);
-                wsc.ds_icon             = wsc.ds_weather.icon;
+                wc.ds_temperature      = wc.ds_weather.temperature;
+                wc.ds_dewpoint         = wc.ds_weather.dewPoint;
+                wc.ds_windBearing      = wc.ds_weather.windBearing;
+                wc.ds_windSpeed        = wc.ds_weather.windSpeed;
+                wc.ds_observation_time = new Date(wc.ds_weather.time);
+                wc.ds_icon             = wc.ds_weather.icon;
                 
                 console.log("Dark Sky"); 
                 console.log(res); 
             }).catch(function(error){
                 console.log(error);
             });        
-    }    
+    };
     
-    wsc.dropdowncitiesSelected = function(city){
-        wsc.selected_city = city;
-        wsc.getLatLonForSelected();
+    wc.dropdowncitiesSelected = function(city){
+        wc.selected_city = city;
+        wc.getLatLonForSelected();
 
-    }
+    };
 
     
     //uses the weatherService to obtain the current conditions
-    wsc.getCurrentConditions = function(){
+    wc.getCurrentConditions = function(){
         
         var google_static_maps_key = "AIzaSyBiUpBjcnq-OEZs8MLzvdijf8BvXBLeMP8";        
         
-        wsc.google_static_maps_url = "https://maps.googleapis.com/maps/api/staticmap?center=" +
-                                     wsc.selected_city.lat + "," +
-                                     wsc.selected_city.lon + 
+        wc.google_static_maps_url = "https://maps.googleapis.com/maps/api/staticmap?center=" +
+                                     wc.selected_city.lat + "," +
+                                     wc.selected_city.lon + 
                                      "&zoom=10&size=600x300&key=" +
                                      google_static_maps_key;
                                      
         console.log("Google Static Map API URL");
-        console.log(wsc.google_static_maps_url);
+        console.log(wc.google_static_maps_url);
         
         //weather underground
-        wsc.getWUCurrentConditions();
+        wc.getWUCurrentConditions();
             
         //darksky
-        wsc.getDSCurrentConditions();
+        wc.getDSCurrentConditions();
         
      
 
     };
     
-    wsc.getLatLonForSelected = function(){
-        GoogleGeolocationService.geoLocate(wsc.selected_city)
+    wc.getLatLonForSelected = function(){
+        GoogleGeolocationService.geoLocate(wc.selected_city)
             .then(function(res){
                 
                 //console.log(res);
 
                 //JSON response object documented here: https://developers.google.com/maps/documentation/geocoding/start
-                wsc.selected_city.lat = res.data.results[0].geometry.location.lat;
-                wsc.selected_city.lon = res.data.results[0].geometry.location.lng;
-                console.log(wsc.selected_city.lat);
-                console.log(wsc.selected_city.lon);
+                wc.selected_city.lat = res.data.results[0].geometry.location.lat;
+                wc.selected_city.lon = res.data.results[0].geometry.location.lng;
+                console.log(wc.selected_city.lat);
+                console.log(wc.selected_city.lon);
       
                 //given asynchronous calling, this must occur WITHIN getLatLonForSelected          
-                wsc.getCurrentConditions();                
+                wc.getCurrentConditions();                
                 
                 
             }).catch(function(error){
@@ -166,33 +167,10 @@ myModule.controller('WeatherServiceController',
     };
     
     //final calls before loading
-    wsc.selected_city = wsc.cities[0];
-    wsc.getLatLonForSelected();
+    wc.selected_city = wc.cities[0];
+    wc.getLatLonForSelected();
     //wsc.getCurrentConditions();
 
-}]);
-
-///// WEATHER UNDERGROUND SERVICE DIRECTIVE /////////////////////////////////////
-myModule.directive('myConditionsWu', ['$sce', function($sce){
-
-    //a reminder on naming conventions for directives: 
-    //https://medium.com/@cironunesdev/angularjs-how-to-name-directives-118ac44b81d4#.idz35zby4
-
-    /* https://docs.angularjs.org/guide/directive
-    The restrict option is typically set to:
-
-    'A' - only matches attribute name
-    'E' - only matches element name
-    'C' - only matches class name
-    'M' - only matches comment
-    */
-    return {
-        restrict: 'E',
-        scope: true,
-        //https://docs.angularjs.org/error/$sce/insecurl
-        //https://docs.angularjs.org/api/ng/service/$sce#trustAsResourceUrl
-        templateUrl: $sce.trustAsResourceUrl('currentConditionsWU.html')
-    }
 }]);
 
 ///// DARKSKY SERVICE DIRECTIVE ////////////////////////////////////////////////
@@ -214,8 +192,8 @@ myModule.directive('myConditionsDs', ['$sce', function($sce){
         scope: true,
         //https://docs.angularjs.org/error/$sce/insecurl
         //https://docs.angularjs.org/api/ng/service/$sce#trustAsResourceUrl
-        templateUrl: $sce.trustAsResourceUrl('currentConditionsDS.html')
-    }
+        templateUrl: $sce.trustAsResourceUrl('DSDirective.html')
+    };
 }]);
 
 ///// GOOGLE GEOLOCATION API SERVICE FACTORY ///////////////////////////////////
@@ -236,7 +214,7 @@ myModule.factory('GoogleGeolocationService',['$sce', '$http', function($sce, $ht
         var trustedurl = $sce.trustAsResourceUrl(url);
         return $http.get(trustedurl);
                   
-    }
+    };
     
     return geolocationService;
     
